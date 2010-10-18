221c20b78cca2d0c915e315ca91427587d648b2b
Mike McCabe <mccabe@archive.org>
2010-10-17 22:18:45 -0700

    changes to make this serve on cluster

--- go.py
+++ go.py
@@ -2,7 +2,7 @@ import sys
 from iabook import *
 from windowed_iterator import windowed_iterator
 import find_pagenos
-import find_header_footer
+# import find_header_footer
 import make_toc
 import json
 
@@ -13,8 +13,20 @@ hfs = False
 
 scandata_ns = ''
 def main(args):
+    doc = ''
+    callback = None
+    if len(args) == 4:
+        (item_id, doc, path, callback) = args
+    elif len(args) == 3:
+        (item_id, doc, path) = args
+    else:
+        print len(args)
+        print args
+        (book_id,) = args
+        path = book_id
+
     book_id = args[0]
-    iabook = Book(book_id, '', book_id)
+    iabook = Book(book_id, doc, path)
     global scandata_ns
     scandata_ns = iabook.get_scandata_ns()
     if djvu:
@@ -28,11 +40,33 @@ def main(args):
     windowed_pages = windowed_iterator(pages, 5, clear_page)
     pages = analyze(windowed_pages)
     toc, qdtoc = make_toc.make_toc(iabook, pages)
-    print '\n'.join(str(ti) for ti in toc)
-    print
-    print '\n'.join(str(ti) for ti in qdtoc)
-    # print json.dumps(toc)
+    if not make_toc.check_toc(qdtoc):
+        qdtoc = []
+
+    # print '\n'.join(str(ti) for ti in toc)
+    # print
+    # print '\n'.join(str(ti) for ti in qdtoc)
+    if callback is not None:
+        print '%s(' % callback
+    print_one_per_line(qdtoc)
+    if callback is not None:
+        print ')'
     # consume(pages)
+
+
+def print_one_per_line(a):
+    def printel(el):
+        return '%s %s -%s- %s' % (el['tocpage'],
+                                  el['pagenum'].rjust(3),
+                                  el['label'],
+                                  el['title'])
+    print '\n'.join(printel(el) for el in a)
+    # print '['
+    # print ',\n'.join(json.dumps(el) for el in a)
+    # # for el in a:
+    # #     print json.dumps(el) + ','
+    # print ']'
+
 
 
 def filter(pages):
@@ -58,8 +92,8 @@ def annotate(pages):
         page.info['bounds'] = page.find_text_bounds()
         if pagenos:
             find_pagenos.annotate_page(page)
-        if hfs:
-            find_header_footer.annotate_page(page)
+        # if hfs:
+        #     find_header_footer.annotate_page(page)
         yield page
         page = None
 
@@ -69,8 +103,8 @@ def analyze(windowed_pages):
         if pagenos:
             find_pagenos.guess_best_pageno(page, windowed_pages,
                                            windowed_pages.window)
-        if hfs:
-            find_header_footer.guess_hf(page, windowed_pages)
+        # if hfs:
+        #     find_header_footer.guess_hf(page, windowed_pages)
         yield page
 
 
@@ -79,8 +113,5 @@ def consume(pages):
         # print page.info
         page = None
 
-
 if __name__ == '__main__':
     main(sys.argv[1:])
-
-
